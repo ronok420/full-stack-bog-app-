e2efd1aad4f789d579fc36cab45526fa1d62f32e
ronok420 <shahariarronok41.com>
2025-04-07 14:01:40 +0600

    live  api  url  added

--- blog-frontend/src/constants/api.js
+++ blog-frontend/src/constants/api.js
@@ -1,1 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000/api";
+// export const API_BASE_URL = "http://localhost:5000/api";
+
+// live  url  link  for backend
+export const API_BASE_URL = "https://blog-server-scrw.onrender.com/api";
